de730345bb2eb217560f94c0981118ae18408ee2
Sam <samsong974@gmail.com>
2020-12-10 18:58:07 -0800

    good compromise

--- pytorch_ex.py
+++ pytorch_ex.py
@@ -46,13 +46,7 @@ class Net(nn.Module):
         #A convolutional layer applies the same (usually small) filter repeatedly at different positions in the layer below it.
 
         self.conv_layrs = nn.Sequential(
-            nn.Conv2d(3, 16, 3, padding = 1),
-            nn.ReLU(),
-            nn.BatchNorm2d(16),
-            nn.MaxPool2d(2, 2),
-            nn.Dropout(0.1),
-
-            nn.Conv2d(16, 32, 3, padding = 1),
+            nn.Conv2d(3, 32, 3),
             nn.ReLU(),
             nn.BatchNorm2d(32),
             nn.MaxPool2d(2, 2),
@@ -61,6 +55,12 @@ class Net(nn.Module):
             nn.Conv2d(32, 64, 3),
             nn.ReLU(),
             nn.BatchNorm2d(64),
+            nn.MaxPool2d(2, 2),
+            nn.Dropout(0.1),
+
+            nn.Conv2d(64, 128, 3),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
             nn.MaxPool2d(2, 2),
             
         )
